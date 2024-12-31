65d5e19eaf887380abaf095326b42e34fdae6aec
Pauline <curtpauline@gmail.com>
2024-12-31 16:03:48 +0100

    feat: install tailwind

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
